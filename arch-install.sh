36abf19ee20d3569ff52a3726a2f94fddbf7b0a8
Zuyav <43708897+Zuyav@users.noreply.github.com>
2019-02-22 13:59:51 +0800

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -2,8 +2,8 @@
 
 host()
 {
-read "hstname?Set hostname:"
-read "rtpswd?Set password for root:"
+read -rp "Set hostname:" hstname
+read -rep "Set password for root:" rtpswd
 
 if [ -z "$usrname" ]; then usrname=admin; fi
 if [ -z "$usrpswd" ]; then usrpswd=admin; fi
